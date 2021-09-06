e668419352485bceb5ee76bca4f8acea75e142b7
Lewis Rotchell <rotchell92@gmail.com>
2021-09-06 20:53:00 +0100

    Boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from "next/head";
+import { Layout } from "../Layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return(
+  <>
+       <Head>
+        <title></title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+   </>)
 }
 export default MyApp
